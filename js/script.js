1535f7a53ce90f15ab8b492231cb7a170eb982b1
SardorQobilov <sardorqobilov555@gmail.com>
2024-03-30 19:19:40 +0500

    dark mode and light mode bugs fix

--- js/script.js
+++ js/script.js
@@ -1,5 +1,46 @@
+// window.addEventListener("DOMContentLoaded", () => {
+//   const colorModeBtn = document.querySelector(".color_mode-btn");
+
+//   let currentImageIndex = 0;
+//   const imageSources = ["images/icons/sun.svg", "images/icons/support.svg"];
+
+//   const loadDarkMode = () => {
+//     const isDarkMode = localStorage.getItem("darkMode") === "true";
+//     if (isDarkMode) {
+//       enableDarkMode();
+//       colorModeBtn.classList.add("active");
+//     }
+//   };
+
+//   const saveDarkMode = () => {
+//     localStorage.setItem("darkMode", document.body.classList.contains("dark"));
+//   };
+
+//   const enableDarkMode = () => {
+//     document.body.classList.add("dark");
+//     saveDarkMode();
+//   };
+
+//   loadDarkMode();
+
+//   colorModeBtn.addEventListener("click", () => {
+//     colorModeBtn.classList.toggle("active");
+//     document.body.classList.toggle("dark");
+//     saveDarkMode();
+//     nextImageSource()
+//   });
+
+//   function nextImageSource() {
+//     currentImageIndex = (currentImageIndex + 1) % imageSources.length;
+//     return imageSources[currentImageIndex];
+//   }
+// });
+
 window.addEventListener("DOMContentLoaded", () => {
   const colorModeBtn = document.querySelector(".color_mode-btn");
+  const imgElement = document.querySelector(".color_mode-btn img");
+
+  const imageSources = ["images/icons/sun.svg", "images/icons/moon.png"];
 
   let currentImageIndex = 0;
 
@@ -9,6 +50,7 @@ window.addEventListener("DOMContentLoaded", () => {
     if (isDarkMode) {
       enableDarkMode();
       colorModeBtn.classList.add("active");
+      imgElement.src = nextImageSource();
     }
   };
 
@@ -23,12 +65,20 @@ window.addEventListener("DOMContentLoaded", () => {
     saveDarkMode();
   };
 
+  // Dark mode ni o'chirish
+  const disableDarkMode = () => {
+    document.body.classList.remove("dark");
+    saveDarkMode();
+  };
+
+  // Sahifani yuklagandan so'ng dark mode holatini yuklash
   loadDarkMode();
 
   colorModeBtn.addEventListener("click", () => {
     colorModeBtn.classList.toggle("active");
+    imgElement.src = nextImageSource();
     document.body.classList.toggle("dark");
-    saveDarkMode();
+    saveDarkMode(); // Har bir tugmachani bosganda dark mode ni saqlash
   });
 
   function nextImageSource() {
