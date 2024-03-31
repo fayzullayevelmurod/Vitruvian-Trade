8461109364b16b6613382dcb35202b393fa1af73
SardorQobilov <sardorqobilov555@gmail.com>
2024-04-01 03:35:31 +0500

    main page media finished

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
   const colorModeBtn = document.querySelector(".color_mode-btn");
+  const parentColorMode = document.querySelector(".header_color-mode");
   const imgElement = document.querySelector(".color_mode-btn img");
 
   const imageSources = ["images/icons/sun.svg", "images/icons/moon.png"];
@@ -26,7 +27,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   loadDarkMode();
 
-  colorModeBtn.addEventListener("click", () => {
+  parentColorMode.addEventListener("click", () => {
     colorModeBtn.classList.toggle("active");
     imgElement.src = nextImageSource();
     document.body.classList.toggle("dark");
@@ -48,3 +49,86 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+//  menu
+const openNavBtn = document.querySelector(".open__nav-btn");
+const closeNavBtn = document.querySelector(".close__nav-btn");
+const nav = document.querySelector("header .nav");
+
+openNavBtn.addEventListener("click", () => {
+  nav.classList.add("active");
+});
+closeNavBtn.addEventListener("click", () => {
+  nav.classList.remove("active");
+});
+
+// reviews swiper
+try {
+  var reviewsSwiper = new Swiper(".reviews-swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    speed: 500,
+    centeredSlides: true,
+    initialSlide: 3,
+    watchOverflow: true,
+    centeredSlides: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-btn-next",
+      prevEl: ".swiper-btn-prev",
+    },
+    breakpoints: {
+      1060: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      460: {
+        slidesPerView: 1.3,
+      },
+      0: {
+        slidesPerView: 1,
+      },
+    },
+  });
+} catch (error) {
+  throw error;
+}
+
+// accordion
+
+const accordion = document.querySelectorAll(".accordion");
+
+if (accordion) {
+  accordion.forEach((item) => {
+    const accordionHeader = item.querySelector(".accordion__header");
+    const accordionContent = item.querySelector(".accordion__content");
+
+    accordionHeader.addEventListener("click", () => {
+      const activeItem = document.querySelector(".accordion.active");
+      if (activeItem && activeItem !== item) {
+        activeItem.classList.remove("active");
+      }
+
+      const activeBody = document.querySelector(
+        ".accordion__content:not(.collapsed)"
+      );
+      if (activeBody && activeBody !== accordionContent) {
+        activeBody.classList.add("collapsed");
+        activeBody.style.height = "0";
+      }
+
+      accordionContent.classList.toggle("collapsed");
+      const isCollapsed = accordionContent.classList.contains("collapsed");
+
+      if (isCollapsed) {
+        accordionContent.style.height = "0";
+        item.classList.remove("active");
+      } else {
+        item.classList.add("active");
+        accordionContent.style.height = accordionContent.scrollHeight + "px";
+      }
+    });
+  });
+}
